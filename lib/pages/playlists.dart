858af515df6b61f09e9667869294e67596452210
Kaelinator <kirkkael@gmail.com>
2018-06-22 15:17:15 -0500

    implemented ListView playlist

--- lib/pages/playlists.dart
+++ lib/pages/playlists.dart
@@ -10,6 +10,15 @@ class PlaylistPageState extends State<PlaylistPage> {
   Widget build(BuildContext context) => new Scaffold(
     appBar: new AppBar(
       title: Text("Playlists")
+    ),
+    body: new ListView.builder(
+      itemCount: 20,
+      itemBuilder: (BuildContext context, int index) {
+        return new ListTile(
+          leading: const Icon(Icons.event_seat),
+          title: new Text('The seat for the narrator $index'),
+        );
+      }
     )
   );
 }
